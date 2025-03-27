4d96379c3cbb9d5b7a361ec32fe77e1541fe12e4
Iroennis Rivas Ramírez <riroenn.is@gmail.com>
2025-03-27 00:04:34 -0400

    Actualizar Configs.py

--- Configs.py
+++ Configs.py
@@ -1,9 +1,9 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 6))
-    API_HASH = os.environ.get("API_HASH", None)
-    TOKEN = os.environ.get("TOKEN", None)
+    APP_ID = int(os.environ.get("APP_ID", 14681595))
+    API_HASH = os.environ.get("API_HASH", "a86730aab5c59953c424abb4396d32d5")
+    TOKEN = os.environ.get("TOKEN", "7725269349:AAGuTEMxnYYre1AA4BcO-_RL7N7Rz-cI3iU")
     BOT_US = os.environ.get("BOT_US", None)
     WELCOME_TEXT = os.environ.get("WELCOME_TEXT", None)
     RULES = os.environ.get("RULES", None)
